053099a5e05a96bd0727019f5f922740ca8bb75a
Vasak <86741983+vasavagyan@users.noreply.github.com>
2021-07-03 11:03:48 +0400

    Add files via upload

---
+++ lesson 4 exsercise 2.cpp
@@ -0,0 +1,6 @@
+/* Lesson 4: Excercise 2: BUG BUSTERS */
+
+int myNumbers[5] = {0};
+myNumbers[5] = 450; // Setting the 5th element to value 450
+
+// 5 tarranoc zangvaci verjin element@ 5-1 indexov e, compile error
---
+++ lesson 4 exsercise 3.cpp
@@ -0,0 +1,6 @@
+/* Lesson 4: Excercise 3: BUG BUSTERS */
+
+int myNumbers[5];
+cout << myNumbers[3];
+
+// printing garbage. printing the element without initializing is a bad decision 

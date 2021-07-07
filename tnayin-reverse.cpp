1991ee880f1a35c8af5ba006512d8bc71af75ff3
Vasak <86741983+vasavagyan@users.noreply.github.com>
2021-07-07 12:23:15 +0400

    Add files via upload

---
+++ tnayin-reverse.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+char* reverse(char*);
+
+int main(){
+  char word[6]= {'h','e','l','l','o','\0'};
+  char* p = reverse(word);
+  for (int i=0; i<5; ++i){
+    std::cout<<p[i];
+  }
+
+  return 0;
+}
+
+char* reverse(char* line){
+  for (int i=0, j=4; i<j; ++i, --j){
+    std::swap(line[i], line[j]);
+  }
+  char* pt = &line[0];
+  return pt;
+}

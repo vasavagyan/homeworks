7001afed9c0b202ced647dcade54169348bd8ac4
Vasak <86741983+vasavagyan@users.noreply.github.com>
2021-09-07 20:05:30 +0400

    Delete lesson 7 exercises directory

--- lesson 7 exercises/lesson 7 exercise 4.cpp
+++
@@ -1,5 +0,0 @@
-BUG BUSTERS: What is wrong with the following function declaration?
-double Area(double Pi = 3.14, double radius);
-
-answer: default parametr@ kam parametrer@ amenaverjum petq a linen,
-        default paramerer@ poxancvum en ajic dzax

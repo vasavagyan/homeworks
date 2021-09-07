144684f876b66506070195cd62003ac85618e8b1
Vasak <86741983+vasavagyan@users.noreply.github.com>
2021-09-07 20:12:02 +0400

    Add files via upload

---
+++ Sorting Algorithms/bubble_sort.cpp
@@ -0,0 +1,16 @@
+void bubble_sort(int* arr, int size){
+    
+    for(int i=0; i<size; ++i){
+        bool have_no_swap = false;
+        for(int j=0; j<size-i-1; ++j){
+            if(arr[j] > arr[j+1]){
+                std::swap(arr[j], arr[j+1]);
+                have_no_swap = false;
+            }
+            if(have_no_swap){
+                break;
+            }
+        }
+    }
+ return ;
+}

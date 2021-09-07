74688351ca53c246b38fd979603b9d4fda1109bf
Vasak <86741983+vasavagyan@users.noreply.github.com>
2021-09-07 20:05:23 +0400

    Delete lesson 5 exercises directory

--- lesson 5 exercises/lesson 5   excercise 1.cpp
+++
@@ -1,6 +0,0 @@
-Lesson 5: exercise 1:
-
-	/* I need to compute the double of a number, 
-           add 5 to it, and then double it again. */
-
-int result = ((number << 1) + 5) << 1;
--- lesson 5 exercises/lesson 5   excercise 2.cpp
+++
@@ -1,7 +0,0 @@
-lesson 5: exercise 2:   What is the value of result stored by this expression:
-int result = number << 1 + 5 << 1;
-
-+_i precedence@ aveli barcr e qan <<_i; hetevabar arajin@ kkatarvi 1+5,
-heto number-@ 6 angam kkrknapatkvi (nuynn e te kbazmapatkvi 64-ov), 
-voric heto evs 1 angam kkrknapatkvi;
- orinak yete number = 5; result = 5 * 64 * 2 = 640

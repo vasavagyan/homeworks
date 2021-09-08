077a321d611909d23d2a1cec647d61190b1036d2
Vasak <86741983+vasavagyan@users.noreply.github.com>
2021-09-08 22:23:34 +0400

    Update bubble_sort.cpp

--- Sorting Algorithms/bubble_sort.cpp
+++ Sorting Algorithms/bubble_sort.cpp
@@ -1,7 +1,7 @@
 void bubble_sort(int* arr, int size){
     
     for(int i=0; i<size; ++i){
-        bool have_no_swap = false;
+        bool have_no_swap = true;
         for(int j=0; j<size-i-1; ++j){
             if(arr[j] > arr[j+1]){
                 std::swap(arr[j], arr[j+1]);
